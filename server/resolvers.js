b5837163a748e02639db94d6fcc7203fc2699f8a
tsutomu.kimura <tsutomu.kimura@nifty.ne.jp>
2022-01-27 02:05:12 +0000

    modify company and add job list

--- server/resolvers.js
+++ server/resolvers.js
@@ -1,6 +1,7 @@
 const db = require('./db')
 
 const Query = {
+    company: (root, args) => db.companies.get(args.id),
     job: (root, args) => db.jobs.get(args.id),
     jobs: (args) => db.jobs.list()
 }
@@ -9,4 +10,9 @@ const Job = {
     company: (job) => db.companies.get(job.companyId)
 }
 
-module.exports = { Query, Job }
+const Company = {
+    jobs: (company) => db.jobs.list()
+    .filter((job) => job.companyId === company.id)
+}
+
+module.exports = { Query, Job, Company }
